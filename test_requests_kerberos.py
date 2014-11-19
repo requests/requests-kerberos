c68bf96a11adbd8c33e1dfc06d250cb246d5799c
Carson Lam <carson.lam@safe.com>
2014-11-18 17:38:40 -0800

    Update 401->200 response test to stop failing, and add a 401->401 response test.

--- test_requests_kerberos.py
+++ test_requests_kerberos.py
@@ -377,6 +377,7 @@ class KerberosTestCase(unittest.TestCase):
 
 
     def test_handle_response_401(self):
+        # Get a 401 from server, authenticate, and get a 200 back.
         with patch.multiple('kerberos',
                             authGSSClientInit=clientInit_complete,
                             authGSSClientResponse=clientResponse,
@@ -404,13 +405,52 @@ class KerberosTestCase(unittest.TestCase):
             response.raw = raw
 
             auth = requests_kerberos.HTTPKerberosAuth()
-            auth.handle_other = Mock(return_value=response_ok)
 
             r = auth.handle_response(response)
 
             self.assertTrue(response in r.history)
-            auth.handle_other.assert_called_with(response_ok)
             self.assertEqual(r, response_ok)
+            self.assertEqual(request.headers['Authorization'], 'Negotiate GSSRESPONSE')
+            connection.send.assert_called_with(request)
+            raw.release_conn.assert_called_with()
+            clientInit_complete.assert_called_with("HTTP@www.example.org")
+            clientStep_continue.assert_called_with("CTX", "token")
+            clientResponse.assert_called_with("CTX")
+            
+    def test_handle_response_401_rejected(self):
+        # Get a 401 from server, authenticate, and get another 401 back.
+        # Ensure there is no infinite recursion.
+        with patch.multiple('kerberos',
+                            authGSSClientInit=clientInit_complete,
+                            authGSSClientResponse=clientResponse,
+                            authGSSClientStep=clientStep_continue):
+                            
+            response_auth_reject = requests.Response()
+            response_auth_reject.url = "http://www.example.org/"
+            response_auth_reject.status_code = 401
+
+            connection = Mock()
+            connection.send = Mock(return_value=response_auth_reject)
+
+            raw = Mock()
+            raw.release_conn = Mock(return_value=None)
+
+            request = requests.Request()
+            response = requests.Response()
+            response.request = request
+            response.url = "http://www.example.org/"
+            response.headers = {'www-authenticate': 'negotiate token'}
+            response.status_code = 401
+            response.connection = connection
+            response._content = ""
+            response.raw = raw
+
+            auth = requests_kerberos.HTTPKerberosAuth()
+
+            r = auth.handle_response(response)
+
+            self.assertTrue(response in r.history)
+            self.assertEqual(r, response_auth_reject)
             self.assertEqual(request.headers['Authorization'], 'Negotiate GSSRESPONSE')
             connection.send.assert_called_with(request)
             raw.release_conn.assert_called_with()
