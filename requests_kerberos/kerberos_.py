9fcb4be515e3ce507e9bab8302443ad41640299e
Carson Lam <carsonyylam@gmail.com>
2016-05-17 00:23:12 -0700

    Fix the fix. Never pass `principal` when using kerberos-sspi, even when None.

--- requests_kerberos/kerberos_.py
+++ requests_kerberos/kerberos_.py
@@ -123,8 +123,11 @@ class HTTPKerberosAuth(AuthBase):
             kerb_spn = "{0}@{1}".format(self.service, kerb_host)
             
             kwargs = {'principal': self.principal}
-            if self._using_kerberos_sspi and self.principal:
-                raise NotImplementedError("Can't use 'principal' argument with kerberos-sspi.")
+            # kerberos-sspi: Never pass principal. Raise if user tries to specify one.
+            if self._using_kerberos_sspi:
+                kwargs = {}
+                if self.principal:
+                    raise NotImplementedError("Can't use 'principal' argument with kerberos-sspi.")
 
             result, self.context[host] = kerberos.authGSSClientInit(kerb_spn,
                 gssflags=gssflags, **kwargs)
