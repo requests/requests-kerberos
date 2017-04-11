88e462bf97f8e157290af252bf4707e31451df10
Jordan Borean <jborean93@gmail.com>
2017-04-11 23:24:58 +1000

    fix for mistake on version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     ],
     extras_require={
         ':sys_platform=="win32"': ['winkerberos>=0.5.0'],
-        ':sys_platform!="win32"': ['kerberos>=1.2.6,<2.0.0'],
+        ':sys_platform!="win32"': ['kerberos>=1.2.5,<2.0.0'],
     },
     test_suite='test_requests_kerberos',
     tests_require=['mock'],
