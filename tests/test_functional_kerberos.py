29dcf74d072eed6df74abe216fbfcfe45de29758
Jordan Borean <jborean93@gmail.com>
2017-06-13 19:50:25 +1000

    Added functional tests and docker Kerberos container

---
+++ tests/test_functional_kerberos.py
@@ -0,0 +1,32 @@
+import requests
+import os
+import unittest
+
+from requests_kerberos import HTTPKerberosAuth, REQUIRED
+
+
+class KerberosFunctionalTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Setup."""
+        self.principal = os.environ.get('KERBEROS_PRINCIPAL', None)
+        self.url = os.environ.get('KERBEROS_URL', None)
+
+        # Skip the test if not set
+        if self.principal is None:
+            raise unittest.SkipTest("KERBEROS_PRINCIPAL is not set, skipping functional tests")
+        if self.url is None:
+            raise unittest.SkipTest("KERBEROS_URL is not set, skipping functional tests")
+
+    def test_jordan(self):
+        session = requests.Session()
+        session.auth = HTTPKerberosAuth(mutual_authentication=REQUIRED, principal=self.principal)
+        request = requests.Request('GET', self.url)
+        prepared_request = session.prepare_request(request)
+
+        response = session.send(prepared_request)
+
+        assert response.status_code == 200, "HTTP response with kerberos auth did not return a 200 error code"
+
+if __name__ == '__main__':
+    unittest.main()
